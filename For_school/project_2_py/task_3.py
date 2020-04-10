a88b8433b5979a4a6d19bf5a00e12b8ef5e8e990
waldis-jr-dev <waldis_jr@outlook.com>
2020-04-10 13:27:35 +0300

    some new files

--- For_school/project_2_py/task_3.py
+++ For_school/project_2_py/task_3.py
@@ -1,7 +1,6 @@
 '''Вивести на екран великі латинські літери, що входять до заданого рядка'''
 
 import string
-str1 = input('Enter string: ')
-for i in str1:
+for i in input('Enter string: '):
     if i in string.ascii_uppercase:
         print(i)
